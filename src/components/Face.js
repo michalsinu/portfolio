840243b727512db811beb280a2495d00c94c6ab0
Michal <michalsinu@gmail.com>
2018-02-18 15:45:06 +0000

    Jumbotron finished with animatio and also mobile-friendly responsiveness, next create other components

--- src/components/Face.js
+++ src/components/Face.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import faceIMG from '../assets/Face.png'
+import Typist from 'react-typist';
+import 'react-typist/dist/Typist.css';
 
 class Face extends Component {
-
   render() {
     return (
       <div>
         <div className="face" />
-        <img src={faceIMG} className="faceIMG" alt="Face" />
-        <div className="faceHeading">
-          Hi, I'm Michal.
-        </div>
+          <img src={faceIMG} className="faceIMG" alt="Face" />
+          <div  className="faceHeading" id="faceHeadingSkeleton">
+            Hi, I'm Michal.
+          </div>
+          <div  className="faceHeading2" id="faceHeadingSkeleton">
+            I create
+          </div>
+          <div  className="faceHeading3" id="faceHeadingSkeleton">
+          <Typist>
+          <Typist.Delay ms={2000} />
+            <span>nice websites.</span>
+          <Typist.Backspace count={14} delay={1000} />
+            <span>good websites.</span>
+          <Typist.Backspace count={14} delay={1000} />
+            <span><b>stunning</b> websites.</span>
+          </Typist>
+          </div>
       </div>
     );
   }
